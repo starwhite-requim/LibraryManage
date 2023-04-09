c5566ad87713492e7a15124695aa20c85d77dc6c
xbb <ruahaha@outlook.com>
2023-04-09 10:40:56 +0800

    修改员工管理页面bug，完成员工管理的所有功能

--- src/main/java/com/se/librarymanagesystem/controller/EmployeeController.java
+++ src/main/java/com/se/librarymanagesystem/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.se.librarymanagesystem.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.se.librarymanagesystem.common.CurrentId;
 import com.se.librarymanagesystem.common.R;
 import com.se.librarymanagesystem.entity.Employee;
@@ -9,12 +10,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.PerformanceSensitive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.awt.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
@@ -38,14 +39,15 @@ public class EmployeeController {
         queryWrapper.eq(Employee::getUsername,employee.getUsername());
         //获取Employee
         Employee empGet = employeeService.getOne(queryWrapper);
-
+        //passowrd进行md5加密
+        String password = DigestUtils.md5DigestAsHex(employee.getPassword().getBytes());
         //3.如果没有查到则返回登陆失败
         if(empGet == null){
             return R.error("登录失败");
         }
 
         //4.如果查询到的密码与传过来的密码不一样，返回登录失败
-        if (!empGet.getPassword().equals(employee.getPassword())){
+        if (!password.equals(empGet.getPassword())){
             return R.error("密码错误");
         }
         //5.查看账号状态，如果为1，则停用
@@ -88,5 +90,96 @@ public class EmployeeController {
         else {
             return R.error("添加失败");
         }
+    }
+
+
+    /**
+     * 登出，清除session中的 ID
+     * @param request  request用于获取session
+     * @return
+     */
+    @PostMapping("/logout")
+    public R logout(HttpServletRequest request){
+        if(request.getSession().getAttribute("Emloyee")!=null){
+            request.getSession().removeAttribute("Employee");
+        }
+        return R.success("登出成功");
+    }
+
+
+    /**
+     *根据用户的输入返回查询结果
+     * 1. 页面发送ajax请求，把分页查询参数（page，pageSize，name）提交到服务端
+     * 2. 服务端Controller接受页面提交的数据并调用Service查询数据
+     * 3. Service调用Mapper操作数据库，查询分页数据
+     * 4. Controller把查询到的分页数据响应给页面
+     * 5. 页面接受到分页数据并通过ElementUI的Table组件展示到页面上
+     * @param page  当前页数
+     * @param pageSize  分页大小
+     * @param name  查询参数，可能不传入
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+        //获取page
+        log.info("page={} pageSize={} name={}",page,pageSize,name);
+
+        //构造分页构造器
+        Page pageInfo = new Page(page,pageSize);
+
+        //构造 条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+        //添加过滤条件，第一个参数为true的时候再执行
+        queryWrapper.like(StringUtils.hasText(name),Employee::getName,name);
+        //添加排序条件
+        queryWrapper.orderByDesc(Employee::getCreateTime);
+        //page方法执行后分页
+        employeeService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
+    }
+
+    /**
+     *  修改员工信息
+     * @param request
+     * @param empIn
+     * @return
+     */
+    @PutMapping
+    public R<String> changeStatus(HttpServletRequest request,@RequestBody Employee empIn){
+        //构造 条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+        //等值查询
+        queryWrapper.eq(Employee::getId,empIn.getId());
+        Employee employee = employeeService.getOne(queryWrapper);
+
+        //设置查询对象的值
+        if (empIn.getName() != null){
+            employee.setName(empIn.getName());
+            employee.setUsername(empIn.getUsername());
+            employee.setSex(empIn.getSex());
+            employee.setIdNumber(empIn.getIdNumber());
+            employee.setPhone(empIn.getPhone());
+        }
+        else {
+            employee.setStatus(empIn.getStatus());
+        }
+        //通过request 请求得到 当前登录的员工ID
+        Long empID = (Long) request.getSession().getAttribute("Employee");
+//        employee.setUpdateTime(LocalDateTime.now());
+//        employee.setUpdateUser(empID);
+        //update
+        employeeService.update(employee,queryWrapper);
+        return R.success("状态修改成功");
+    }
+
+    /**
+     * Restful风格请求，获得相应id的Employee实体
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<Employee> getEmployee(@PathVariable Long id){
+        Employee employee = employeeService.getById(id);
+        return R.success(employee);
     }
 }
